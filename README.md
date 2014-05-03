cf7b326bb1c81b745573852712e56ce0374e4767
Michael Williams <dinosaur@riseup.net>
2014-05-03 16:59:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# geojson\-mapify
+
+map geojson using leafletjs
+
+## how to
+
+### install
+
+```
+npm install --save geojson\-mapify
+```
+
+### use
+
+```
+var geojson\-mapify = require('geojson\-mapify');
+```
+
+## license
+
+ISC
---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = function (options) {
+  var map = new L.Map(options.map, options.mapOptions);
+
+  var geoJson = L.geoJson(options.geoJson, options.geoJsonOptions);
+
+  map.fitBounds(geoJson.getBounds().pad(0.05))
+  map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+  
+  geoJson.addTo(map); 
+
+  // TODO figure out
+  //var markers = new L.MarkerClusterGroup();
+  //markers.addLayer(communityGeo);
+  //map.addLayer(markers);
+  
+  return map;
+};
